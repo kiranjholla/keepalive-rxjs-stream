19f1ccf307660ad53bb100ba1fb50e140839d59e
KH <4491818+kiranjholla@users.noreply.github.com>
2023-02-10 14:15:21 +0530

    Implementation of a keep-alive operator

--- keep-alive.ts
+++ keep-alive.ts
@@ -1,9 +1,27 @@
-import { Subscriber } from 'rxjs';
+import { Observable, ObservableInput } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
-import { Subscriber } from 'rxjs';
-
-export class KeepStreamAliveSubscriber<T> extends Subscriber<T> {
-  constructor(subscriber: Subscriber<T>) {
-    super(subscriber);
-  }
+export function keepStreamAlive({
+  retries = Infinity,
+  notifyStop = () => Promise.resolve(),
+  resetOnSuccess = true
+}: Partial<{
+  retries: number;
+  notifyStop: () => ObservableInput<any>;
+  resetOnSuccess: boolean;
+}> = {}) {
+  return (source$: Observable<any>) =>
+    new Observable(subscriber =>
+      source$.subscribe({
+        next: value => subscriber.next(value),
+        error: error => subscriber.error(error),
+        complete: () => subscriber.error('complete')
+      })
+    ).pipe(
+      retry({
+        count: retries,
+        delay: notifyStop,
+        resetOnSuccess
+      })
+    );
 }
